2fc85f23b9048ab31b691ff67147d6d87f713137
Daviiduhh <daviiduhhs@gmail.com>
2022-01-24 15:21:28 -0600

    Iconos agregados y header mobile terminado

---
+++ components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+  <header class="header">
+    <img class="header__logo" src="../assets/aerolab-logo.svg" alt="Logo aerolab" />
+    <div class="header__points">
+      <img
+        class="header__points__logo"
+        src="../assets/icons/aerolab-logo-white.svg"
+        alt="Aerolab logo points"
+      />
+      <p class="header__points__points gradient-text">1000</p>
+      <img class="header__points__arrow" src="../assets/icons/arrow-down.svg" alt="Arrow icon">
+    </div>
+  </header>
+</template>
+
+<style>
+.header {
+  width: 100%;
+  height: 128px;
+  padding: 0 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.header__points {
+  width: 143px;
+  height: 40px;
+  box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.05);
+  border-radius: 16px;
+  border: 1px solid #dae4f2;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.header__points__logo {
+    width: 24px;
+    height: 24px;
+}
+
+.header__points__points {
+    font-weight: 600;
+}
+
+.header__points__arrow {
+    width: 15px;
+    height: 7.5px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Tutorial />
+  <Header />
 </template>
 
 <script>
@@ -7,3 +7,14 @@ export default {
   name: 'IndexPage',
 }
 </script>
+
+<style>
+.gradient-text {
+  background: #176feb;
+  background: -webkit-linear-gradient(to right, #176feb 0%, #ff80ff 100%);
+  background: -moz-linear-gradient(to right, #176feb 0%, #ff80ff 100%);
+  background: linear-gradient(to right, #176feb 0%, #ff80ff 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+</style>
