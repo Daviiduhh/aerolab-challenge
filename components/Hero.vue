56742cda1cd1cb7b332feb8d43a10ef1e5e6dfd0
Daviiduhh <daviiduhhs@gmail.com>
2022-01-24 20:16:03 -0600

    Estilos globales e implementacion de hero

---
+++ components/Hero.vue
@@ -0,0 +1,61 @@
+<template>
+    <section class="hero contenedor">
+        <p class="hero__subtitle">explore the</p>
+        <h1 class="hero__title gradient-text">tech <span class="hero__title--span">zone</span></h1>
+        <p class="hero__text">Here you'll be able to redeem all of your hard-earned Aeropoints and exchange them for cool tech.</p>
+        <button class="hero__button gradient btn">
+            <p class="hero__button__text">view all products</p>
+            <img class="hero__button__arrow" src="../assets/icons/full-arrow.svg" alt="View all products icon">
+        </button>
+    </section>
+</template>
+
+<style>
+.hero {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+
+    /* Waves */
+    background-repeat: no-repeat;
+    background-position: bottom;
+    background-size: cover;
+}
+
+.hero__subtitle {
+    text-transform: uppercase;
+    margin-bottom: 8px;
+    line-height: 24px;
+}
+
+.hero__title {
+    text-align: center;
+    margin-bottom: 24px;
+}
+.hero__title--span {
+    -webkit-text-fill-color: #252f3d;
+}
+
+.hero__text {
+    text-align: center;
+    margin-bottom: 40px ;
+}
+
+.hero__button {
+    height: 64px;
+    padding: 20px 48px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 10px;
+}
+.hero__button__text {
+    text-transform: uppercase;
+    font-size: 16px;
+    color: #fff;
+}
+.hero__button--arrow {
+    display: inline-block;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <Header />
+  <main>
+    <Header />
+    <Hero />
+  </main>
 </template>
 
 <script>
@@ -9,6 +12,65 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;900&display=swap');
+
+body {
+  color: #7C899C;
+  font-size: 16px;
+  font-family: 'Montserrat', sans-serif;
+  line-height: 24px;
+}
+
+h1 {
+  font-size: 96px;
+  font-weight: 900;
+  line-height: 80%;
+  letter-spacing: 0%;
+  text-transform: uppercase;
+}
+h2 {
+  font-size: 32px;
+  font-weight: 900;
+  line-height: 80%;
+  letter-spacing: 0%;
+  text-transform: uppercase;
+}
+h3 {
+  font-size: 24px;
+  font-weight: 900;
+  line-height: 100%;
+  letter-spacing: 0%;
+  text-transform: uppercase;
+}
+p {
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 150%;
+  letter-spacing: 0%;
+}
+
+.contenedor {
+  padding: 30px 20px;
+}
+.gradient {
+  background: rgb(23, 111, 235);
+  background: -moz-linear-gradient(
+    102deg,
+    rgba(23, 111, 235, 1) 0%,
+    rgba(255, 128, 255, 1) 100%
+  );
+  background: -webkit-linear-gradient(
+    102deg,
+    rgba(23, 111, 235, 1) 0%,
+    rgba(255, 128, 255, 1) 100%
+  );
+  background: linear-gradient(
+    102deg,
+    rgba(23, 111, 235, 1) 0%,
+    rgba(255, 128, 255, 1) 100%
+  );
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#176feb",endColorstr="#ff80ff",GradientType=1);
+}
 .gradient-text {
   background: #176feb;
   background: -webkit-linear-gradient(to right, #176feb 0%, #ff80ff 100%);
@@ -17,4 +79,16 @@ export default {
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
 }
+.btn {
+  border: none;
+  border-radius: 12px ;
+  font-weight: 600;
+}
+.btn:hover {
+  opacity: 0.9;
+}
+.btn:active {
+  transform: scale(0.9);
+}
+
 </style>
